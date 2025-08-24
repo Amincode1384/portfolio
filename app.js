2dfed468aa6e4dc9dd5f0b8e1dc04269369eaee6
mohammad amin <izadi.mohammad.1384@gmail.com>
2025-08-24 13:43:36 +0330

    responsiv the website part1

---
+++ app.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".menu");
+let menubtn = document.querySelector(".header-icon");
+let menubtnicon = document.querySelector(".header-icon",i);
+
+menubtn.addEventListener("click", function() {
+    menu.style.left = "0";
+});
+console.log("amiviii")
